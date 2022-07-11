c409f1edd12f5cb71ab7d4caa7ccf22ed8c3f669
Lu Ji <lu.ji1@me.com>
2022-07-11 05:08:34 -0400

    Bugfix

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -7,23 +7,26 @@ import Lobby from './pages/lobby';
 import Room from './pages/room';
 import { Toaster } from 'react-hot-toast';
 
-const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
-root.render(
-  <>
-    <Toaster
-      toastOptions={{
-        style: {
-          borderRadius: '10px',
-          background: '#333',
-          color: '#fff',
-        },
-      }}
-    />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Lobby />} />
-        <Route path="/room/:room_name" element={<Room />} />
-      </Routes>
-    </BrowserRouter>
-  </>
-);
+const appDiv = document.getElementById('app') as HTMLElement;
+if (appDiv) {
+  const root = ReactDOM.createRoot(appDiv);
+  root.render(
+    <>
+      <Toaster
+        toastOptions={{
+          style: {
+            borderRadius: '10px',
+            background: '#333',
+            color: '#fff',
+          },
+        }}
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Lobby />} />
+          <Route path="/room/:room_name" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
